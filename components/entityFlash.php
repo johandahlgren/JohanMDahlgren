e058079882a75fdc9a049e40a53bb9b185c5f2b4
Johan Dahlgren <johan.dahlgren@knowit.se>
2014-03-11 08:54:27 +0100

    Diverse layoutfixar mm.

--- components/entityFlash.php
+++ components/entityFlash.php
@@ -1,7 +1,8 @@
 <a href="index.php?entityId=<?php print getValueFromString("DetailPage", $data) ?>">
 	<div class="entityFlash <?php print getValueFromString("ExtraClass", $data) ?>">
-        <div class="entityFlashImage" style="background-image: url(<?php print getValueFromString("Image", $data) ?>);"></div>    
+        <div class="entityFlashImage" style="background-image: url(<?php print getValueFromString("Image", $data) ?>);"></div>
         <h2><?php print $name ?></h2>
         <p><?php print htmlspecialchars_decode(getValueFromString("Text", $data)) ?></p>
+        <span class="fakeLink">Read more &raquo;</span>
 	</div>
 </a>
