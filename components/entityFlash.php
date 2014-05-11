333481f966cc47538d121128c7734e0752d97414
Johan <johanmdahlgren@gmail.com>
2014-05-11 11:05:11 +0200

    Adds stuff.

--- components/entityFlash.php
+++ components/entityFlash.php
@@ -1,9 +1,7 @@
 <a href="index.php?entityId=<?php print getValueFromString("DetailPage", $data) ?>">
 	<div class="entityFlash <?php print getValueFromString("ExtraClass", $data) ?>">
+        <div class="entityFlashImage" style="background-image: url(<?php print getValueFromString("Image", $data) ?>);"></div>
         <h2><?php print $name ?></h2>
-        <?php if (getValueFromString("Image", $data) !="" ) { ?>
-            <img src="<?php print getValueFromString("Image", $data) ?>" />
-        <?php } ?>
         <p><?php print htmlspecialchars_decode(getValueFromString("Text", $data)) ?></p>
         <span class="fakeLink">Read more &raquo;</span>
 	</div>
