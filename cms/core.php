7edc3e5802a7fdea5a5a8ec9c27a230ccaf3f608
Johan Dahlgren <johan.dahlgren@knowit.se>
2014-11-25 17:32:27 +0100

    Fixes stuff.

--- cms/core.php
+++ cms/core.php
@@ -888,4 +888,34 @@
 			}
 		}
 	}
+
+	function insertComments($id) {
+		?>
+			<div id="comments<?php print $id ?>" class="comments">
+				<?php
+					ob_start();
+					renderEntitiesList($id, null, null, "DESC", 999);
+					$comments = ob_get_clean();
+
+					if ($comments == "") {
+						print "No comments yet.";
+					} else {
+						print $comments;
+					}
+				?>
+				<div id="toggleCommentFields<?php print $id ?>" class="toggleCommentFields" onclick="toggleCommentFields(<?php print $id ?>)">Write comment</div>
+				<div id="insertComment<?php print $id ?>" class="insertComment">
+					<form>
+						<label for="commenterName<?php print $id ?>">Name <span class="labelDescription">(Enter your name)</span></label><input type="text" id="commenterName<?php print $id ?>" name="commenterName<?php print $id ?>"/><br/>
+						<label for="commentText<?php print $id ?>">Message  <span class="labelDescription">(Enter your message)</span></label><textarea id="commentText<?php print $id ?>" name="commentText<?php print $id ?>"></textarea><br/>
+						<div class="center">
+							<div class="button cancelComment" onclick="toggleCommentFields(<?php print $id ?>);">Cancel</div>
+							<div class="button postComment" onclick="postComment(<?php print $id ?>);">Post</div>
+						</div>
+					</form>
+				</div>
+                <a href="index.php?entityId=<?php print $_REQUEST["detailPage"] ?>&subEntityId=<?php print $id ?>" class="readMore">More</a>
+			</div>
+		<?php
+	}
 ?>
--- components/news_list.php
+++ components/news_list.php
@@ -1,29 +1,13 @@
-<article>
-    <div class="divider"></div><div class="date"><time datetime="<?php print getYMDM2M($publishDate) ?>"><?php print getYMDshort($publishDate) ?></time></div><div class="divider"></div>
-    <h2><?php print $name ?></h2>
-    <?php print formatText(getValueFromString( "Text", $data)); ?>
-    <div id="comments<?php print $id ?>" class="comments">
-        <?php
-    ob_start();
-renderEntitiesList($id, null, null, "DESC", 999);
-$comments = ob_get_clean();
-
-if ($comments == "") {
-    print "No comments yet.";
-} else {
-    print $comments;
-}
-        ?>
-    </div>
-    <div id="toggleCommentFields<?php print $id ?>" class="toggleCommentFields" onclick="toggleCommentFields(<?php print $id ?>)">Write comment</div>
-    <div id="insertComment<?php print $id ?>" class="insertComment">
-        <form>
-            <label for="commenterName<?php print $id ?>">Name <span class="labelDescription">(Enter your name)</span></label><input type="text" id="commenterName<?php print $id ?>" name="commenterName<?php print $id ?>"/><br/>
-            <label for="commentText<?php print $id ?>">Message  <span class="labelDescription">(Enter your message)</span></label><textarea id="commentText<?php print $id ?>" name="commentText<?php print $id ?>"></textarea><br/>
-            <div class="center">
-                <div class="button cancelComment" onclick="toggleCommentFields(<?php print $id ?>);">Cancel</div>
-                <div class="button postComment" onclick="postComment(<?php print $id ?>);">Post</div>
-            </div>
-        </form>
-    </div>
+<article id="article<?php print $id ?>" itemscope itemtype="http://schema.org/BlogPosting">
+    <div class="divider"></div><div class="date"><time datetime="<?php print getYMDM2M($publishDate) ?>" itemprop="dateCreated"><?php print getYMDshort($publishDate) ?></time></div><div class="divider"></div>
+    <h2 itemprop="name"><?php print $name ?></h2>
+	<?php if (getValueFromString("Image", $data) != "") { ?>
+		<a href="<?php print getValueFromString("Image", $data) ?>" class="newsImageLink colorbox" title="<?php print getValueFromString("ImageText", $data) ?>">
+			<span class="newsImage" style="background-image: url(<?php print getValueFromString("Image", $data) ?>);" ></span>
+		</a>
+	<?php } ?>
+	<span itemprop="articleBody">
+		<?php print formatText(getValueFromString( "Text", $data)); ?>
+	</span>
+    <?php insertComments($id); ?>
 </article>
